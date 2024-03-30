c3a20ea177d08f114c1708162ec0fef696e81fbf
Elefantt <164626984+Elefantt@users.noreply.github.com>
2024-03-30 11:50:49 +0200

    Create menu

---
+++ Assets/Script/VolumeController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+    private AudioSource volumeSource;
+    void Start()
+    {
+        volumeSource = GetComponent<AudioSource>();
+        volumeSlider.value = volumeSource.volume;
+    }
+
+    public void ChangeVolume()
+    {
+        volumeSource.volume = volumeSlider.value;
+    }
+}
