a15e00836bae8984989a73dff50cadd6b414e15a
barduuuur <illyakoval2009@gmail.com>
2024-04-06 13:15:54 +0300

    lesson

--- Assets/Script/Shooting.cs
+++ Assets/Script/Shooting.cs
@@ -61,7 +61,7 @@ public class Shooting : MonoBehaviour
             fireObject.transform.LookAt(target.transform.position);
 
             Rigidbody rb = PrefabBolled.GetComponent<Rigidbody>();
-            rb.AddForce(fireObject.forward * 11f, ForceMode.Impulse);
+            rb.AddForce(fireObject.forward * 15f, ForceMode.Impulse);
             
             yield return new WaitForSeconds(1f);
         }
