c04cf9069c95b3d3e10939a7b621dce2afc68efb
SteveLOv <166212359+SteveLOv@users.noreply.github.com>
2024-04-07 01:17:12 +0300

    towerBuild

---
+++ Assets/BuildTower/Building.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public Vector2Int Size = Vector2Int.one;
+
+    private void OnDrawGizmosSlected()
+    {
+        for (int x = 0; x < Size.x; x++)
+        {
+            for (int y = 0; y < Size.y; y++)
+            {
+                Gizmos.color = Color.gray;
+                Gizmos.DrawCube(transform.position + new Vector3(x, 0, y), new Vector3(1, 1f, 1));
+            }
+        }
+    }
+}
