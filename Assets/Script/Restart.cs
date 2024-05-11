495a7db28f5115cfdfb8474aba2b38822a1f69de
Elefantt <164626984+Elefantt@users.noreply.github.com>
2024-05-11 12:18:41 +0300

    drg

---
+++ Assets/Script/Restart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Restart : MonoBehaviour
+{
+    public void RestartLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void RestartLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void RestartLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+}
